bee7e3fc682b6d58565fa64fe4894f6527f6f1a1
William SCHILLER <william.schiller.07@gmail.com>
2021-04-05 20:57:02 +0200

    Init project

---
+++ src/Controller/MainController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class MainController
+ * @package App\Controller
+ * @Route(path="", name="main_")
+ */
+class MainController extends AbstractController
+{
+    /**
+     * @Route("", name="home")
+     */
+    public function home(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+}
