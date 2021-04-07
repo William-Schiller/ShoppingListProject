b8eb949c91bd1faba068f28232f68d140d0ccf5a
William SCHILLER <william.schiller.07@gmail.com>
2021-04-07 23:29:11 +0200

    up

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,13 +4,11 @@ namespace App\Controller;
 
 use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
-use Sunrise\Slugger\SluggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use function Sodium\add;
 
 /**
  * Class UserController
@@ -23,7 +21,8 @@ class UserController extends AbstractController
     /**
      * @Route("/update", name="update")
      */
-    public function update(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+    public function update(Request $request, EntityManagerInterface $entityManager //, SluggerInterface $slugger
+    ): Response
     {
         $user = $this->getUser();//on recupere celui qui se log
 
@@ -36,17 +35,18 @@ class UserController extends AbstractController
             //debut test
             if($image){
                 $originalFileName= pathinfo($image->getClientOriginalName(),PATHINFO_FILENAME);
-                $safeFileName=$slugger->slugify($originalFileName, '.');
-                $newFileName = $safeFileName.'-'.uniqid().'.'.$image->guessExtension();
+//                $safeFileName=$slugger->slugify($originalFileName, '.');
+//                $newFileName = $safeFileName.'-'.uniqid().'.'.$image->guessExtension();
+                $newFileName = $originalFileName;
                 try {
                     $image->move(
                         $this->getParameter('picture_profile_directory'),
-                        $newFileName
+                        $newFileName . '.jpg' //Je triche sur .jpg
                     );
                 }catch (FileException $e){
                     //TODO traiter l exception
                 }
-                $user->setPictureFileName($newFileName);
+                $user->setPictureFileName($newFileName . '.jpg'); // Je triche car fonction guessExtension pose probleme
             }
             //fin test , ensuite configu services yaml parameters
 
